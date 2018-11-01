b35d02f6ebe278b93574ef75e0189da6bd1ce759
Rafaela Santos <faelaphoenix@users.noreply.github.com>
2018-10-31 21:40:20 -0300

    Update urls.py

--- blog/urls.py
+++ blog/urls.py
@@ -1,9 +1,7 @@
-from django.conf.urls import include, url
+from django.urls import path
 from . import views
 
 
 urlpatterns = [
-    url(r'^$', views.post_list),
-    url(r'^post/(?P<pk>[0-9]+)/$', views.post_detail),
-    url(r'^post/new/$', views.post_new, name='post_new'),
+    path('', views.post_list, name='post_list'),
 ]
